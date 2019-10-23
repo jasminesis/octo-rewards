7edab25c78a94df4774fdc1bfe39b66a1c260336
jasminesis <jasmine-aips@hotmail.com>
2019-10-23 15:58:05 +0800

    styled expenses a little

--- views/expenses/index.jsx
+++ views/expenses/index.jsx
@@ -3,32 +3,25 @@ var Layout = require('../defaultlayout');
 
 class Expenses extends React.Component {
 	render() {
-		console.log("From the index.jsx file", this.props);
-		const expenses = this.props.result.map(el => {
+		console.log('From the index.jsx file', this.props);
+		const expenses = this.props.result.map((el) => {
 			return (
-				<div className='card-5'>
-						<div className='card-body'>
-							<h5 className='card-title'>Expense</h5>
-							<p className="card-text">
-								Amount: {el.amount} <br/>
-								Category: {el.category} <br/>
-								Card: {el.card_id} <br/>
-							</p>
-						</div>
-					</div>
-			)
-		})
-		return (
-			<Layout>
-				<div className='text-center col-12 d-flex flex-wrap justify-content-center'>
-					<div className='card-5 col-6'>
-						<img src='octopus.jpeg' className='card-img-top col-5' alt='...' />
-						<div className='card-body'>
-							<h5 className='card-title'>All expenses</h5>
-							{expenses}
-						</div>
+				<div className='card col-5 m-2'>
+					<div className='card-body'>
+						<h5 className='card-title'>Expense</h5>
+						<p className='card-text'>
+							Amount: ${el.amount} <br />
+							Category: {el.category} <br />
+							Card: {el.card_id} <br />
+						</p>
 					</div>
 				</div>
+			);
+		});
+		return (
+			<Layout>
+				<h1 className='text-center m-3 display-3'>All expenses</h1>
+				<div className='text-center col-12 d-flex flex-wrap justify-content-center'>{expenses}</div>
 			</Layout>
 		);
 	}
