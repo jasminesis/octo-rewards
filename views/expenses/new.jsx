2b033a72a7eedccbc6745e21198555615776a516
jasminesis <jasmine-aips@hotmail.com>
2019-10-25 16:33:42 +0800

    styled forms in new expense using bootstrap

--- views/expenses/new.jsx
+++ views/expenses/new.jsx
@@ -5,50 +5,57 @@ class NewExpenses extends React.Component {
 	render() {
 		return (
 			<Layout>
-				<div className='text-center col-12 d-flex flex-wrap justify-content-center'>
+				<div className='col-12 d-flex flex-wrap justify-content-center'>
 					<div className='card-5 col-6'>
 						<div className='card-body'>
-							<h1 className='card-title'>New expense</h1>
+							<h1 className='card-title text-center'>New expense</h1>
 							<p>
-								<form className='m-3 justify-content-end' method='POST' action='/expenses'>
-									Amount: <input type='text' name='username' required />
-									<br />
-									<br />
-									Category:{' '}
-									<select name='category'>
-										<option>Select a Category...</option>
-										<option value='Beauty'>Beauty & Wellness</option>
-										<option value='Dining'>Dining</option>
-										<option value='Entertainment'>Entertainment</option>
-										<option value='Family'>Family</option>
-										<option value='Fashion'>Fashion</option>
-										<option value='Transport'>Transport</option>
-										<option value='Travel'>Travel</option>
-									</select>
-									<br />
-									<br />
-									Payment Method: {' '}
-									<select id="paymentMethod" name='payment_method'>
-										<option>Select an option...</option>
-										<option value='card'>Card</option>
-										<option value='cash'>Cash</option>
-									</select>
-									<br />
-									<br />
-									Card: {' '}
-									<select id="showAllCards" name='card_id'>
-										<option value='null'></option>
-										<option value='card'>Card</option>
-									</select>
-									<br />
-									<br />
+								<form className='m-3' method='POST' action='/expenses'>
+									<div class='form-group'>
+										<label for='exampleFormControlInput1'>Amount</label>
+										<input
+											type='text'
+											class='form-control'
+											id='exampleFormControlInput1'
+											name='amount'
+											required
+										/>
+									</div>
+
+									<div className='form-group'>
+										<label for='exampleFormControlSelect1'>Category</label>
+										<select class='form-control' name='category'>
+											<option>Select a Category...</option>
+											<option value='Beauty'>Beauty & Wellness</option>
+											<option value='Dining'>Dining</option>
+											<option value='Entertainment'>Entertainment</option>
+											<option value='Family'>Family</option>
+											<option value='Fashion'>Fashion</option>
+											<option value='Transport'>Transport</option>
+											<option value='Travel'>Travel</option>
+										</select>
+									</div>
+									<div className='form-group'>
+										<label>Payment Method</label>
+										<select class='form-control' name='payment_method' id='paymentMethod'>
+											<option>Select an option...</option>
+											<option value='card'>Card</option>
+											<option value='cash'>Cash</option>
+										</select>
+									</div>
+									<div className='form-group'>
+										<label for='exampleFormControlSelect1'>Card</label>
+										<select class='form-control' id='showAllCards' name='card_id'>
+											<option>Select an option...</option>
+										</select>
+									</div>
 									<input type='submit' value='Submit' className='m-2' />
 								</form>
 							</p>
 						</div>
 					</div>
 				</div>
-				<script src="/scriptofcards.js"></script>
+				<script src='/scriptofcards.js' />
 			</Layout>
 		);
 	}
