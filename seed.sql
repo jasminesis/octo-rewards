ca4bfd93368f5256d5fa1880684e57c41924bd7b
Jasmine <jasmine-aips@hotmail.com>
2019-10-26 22:07:44 +0800

    added 1 to many table for card thresholds

--- seed.sql
+++ seed.sql
@@ -1,10 +1,48 @@
-INSERT INTO expenses (user_id, amount, category, payment_method, card_id) VALUES 
-(1, 20, 'Dining', 'card', 2), 
-(1, 40, 'Dining', 'card', 2), 
-(1, 60, 'Dining', 'card', 2);
-INSERT INTO cards (bank, name, description, type, min_spend, max_spend, base_rate, next_rate, billing_date) VALUES 
-('UOB', 'Lady''s Card', 'You''ll earn 10X UNI$ (or 20 miles) per S$5 spent on your chosen category(ies) with no minimum spend required.', 'Rewards', 0, 1000, 0.4, 4, 20),
-('UOB', 'One', 'Great rebate card', 'Cashback', 500, 0, 0.03333, 0.05, 29 );
-INSERT INTO cards (bank, name, description, type, min_spend, max_spend, base_rate, next_rate, billing_date) VALUES ('Citi', 'PremierMiles', 'Great foreign currency earnings', 'Rewards', 0, 3000, 1.2, 1.5, 7);
-
-INSERT INTO card_owners(user_id, card_id) VALUES (1, 1);
+INSERT INTO expenses (
+    user_id,
+    amount,
+    category,
+    payment_method,
+    card_id
+  )
+VALUES
+  (1, 20, 'Dining', 'card', 1),
+  (1, 20, 'Fashion', 'card', 2),
+  (1, 40, 'Dining', 'card', 2),
+  (1, 60, 'Dining', 'card', 2);
+INSERT INTO cards (bank, name, description, type, billing_date)
+VALUES
+  (
+    'UOB',
+    'Lady''s Card',
+    'You''ll earn 10X UNI$ (or 20 miles) per S$5 spent on your chosen category(ies) with no minimum spend required.',
+    'Rewards',
+    20
+  ),
+  (
+    'UOB',
+    'One',
+    'Great rebate card',
+    'Cashback',
+    29
+  ),
+  (
+    'Citi',
+    'PremierMiles',
+    'Great foreign currency earnings',
+    'Rewards',
+    7
+  );
+INSERT INTO card_owners (user_id, card_id)
+VALUES
+  (1, 1),
+  (1, 2)
+INSERT INTO card_rates (card_id, spend_threshold, rate)
+VALUES
+  (1, 0, 0.4),
+  (1, 1000, 4),
+  (2, 500, 0.0333),
+  (2, 1000, 0.0333),
+  (2, 2000, 0.05),
+  (3, 0, 1.2),
+  (3, 3000, 1.5);
--- tables.sql
+++ tables.sql
@@ -1,31 +1,33 @@
 CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    username TEXT,
-    password TEXT
+  id SERIAL PRIMARY KEY,
+  username TEXT,
+  password TEXT
 );
 CREATE TABLE IF NOT EXISTS expenses (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER,
-    date_added DATE NOT NULL DEFAULT CURRENT_DATE,
-    amount FLOAT,
-    category TEXT,
-    payment_method TEXT,
-    card_id INTEGER
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER,
+  date_added DATE NOT NULL DEFAULT CURRENT_DATE,
+  amount FLOAT,
+  category TEXT,
+  payment_method TEXT,
+  card_id INTEGER
 );
 CREATE TABLE IF NOT EXISTS cards (
-    id SERIAL PRIMARY KEY,
-    bank TEXT,
-    name TEXT UNIQUE,
-    description TEXT,
-    type TEXT,
-    min_spend INTEGER,
-    max_spend INTEGER,
-    base_rate FLOAT,
-    next_rate FLOAT,
-    billing_date INTEGER
+  id SERIAL PRIMARY KEY,
+  bank TEXT,
+  name TEXT UNIQUE,
+  description TEXT,
+  type TEXT,
+  billing_date INTEGER
 );
 CREATE TABLE IF NOT EXISTS card_owners (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER,
-    card_id INTEGER
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER,
+  card_id INTEGER
+);
+CREATE TABLE IF NOT EXISTS card_rates (
+  id SERIAL PRIMARY KEY,
+  card_id INTEGER,
+  spend_threshold INTEGER,
+  rate FLOAT
 );
