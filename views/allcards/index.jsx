6c56dfb16cf7cd9e4c36cd446cbe99ce412564c0
Jasmine <jasmine-aips@hotmail.com>
2019-10-28 15:01:19 +0800

    displays allCARDS

--- views/allcards/index.jsx
+++ views/allcards/index.jsx
@@ -4,25 +4,42 @@ var Layout = require('../defaultlayout');
 class AllCards extends React.Component {
     render() {
         console.log('From the index.jsx file for all cards', this.props);
-        const cards = this.props.result.map((el) => {
+        const newRow = this.props.result.map((el, index) => {
             return (
-                <div className='card col-8 m-2'>
-                    <div className='card-body'>
-                        <h5 className='card-title'>{el.bank} {el.name}</h5>
-                        <p className='card-text'>
-                            Description: {el.description} <br />
-                            Type: {el.type} <br />
-                            Category based: {' ' + el.category_based} <br />
-                            Maximum: {el.unit}{el.max} <br />
-                        </p>
-                    </div>
-                </div>
+
+                <tr>
+                    <th scope="row">{el.id}</th>
+                    <td>{el.bank}</td>
+                    <td>{el.name}</td>
+                    <td>{el.description}</td>
+                    <td>{el.type}</td>
+                    <td>{' ' + el.category_based}</td>
+                    <td>{el.unit} {el.max}</td>
+                </tr>
+
             );
         });
         return (
             <Layout>
                 <h1 className='text-center m-3 display-3'>All cards</h1>
-                <div className='text-center col-12 d-flex flex-wrap justify-content-center'>{cards}</div>
+                <div className='d-flex justify-content-center'>
+                    <table class="table col-10">
+                        <thead class="thead-light">
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Bank</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Type</th>
+                                <th scope="col">Category based?</th>
+                                <th scope="col">Maximum cap</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {newRow}
+                        </tbody>
+                    </table>
+                </div>
             </Layout>
         );
     }
